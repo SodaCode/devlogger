935fb444868f4d8aef670b578240708cad074ae2
SodaCode <dominicsaechao1@gmail.com>
2019-11-26 20:04:31 -0800

    Made the log.text.values appear in the form when clicked. Changed the logService to an observable so that the information will load ASYNC. Added ngModel in the log-form html to display text from clicked logitem

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../services/log.service';
 
 import { Log } from '../../models/Log';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-logs',
@@ -15,7 +16,13 @@ export class LogsComponent implements OnInit {
   constructor(private logService: LogService) { }
 
   ngOnInit() {
-    this.logs = this.logService.getLogs();
+    this.logService.getLogs().subscribe(logs => {
+      this.logs = logs;
+    });
+  }
+
+  onSelect(log: Log) {
+    this.logService.setFormLog(log);
   }
 
 }
